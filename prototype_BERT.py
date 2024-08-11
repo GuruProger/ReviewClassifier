ce5e881b7ac5c21f230c87e95ad53edd4e505ce6
GuruProger <progerguru@gmail.com>
2024-08-11 12:06:24 +0300

    style: updating comments and improving the structure of the code in prototype_BERT.py
    pdating comments, formatting the code according to the PEP 8 standard, removing unnecessary ones .astype(str) for better code readability

--- prototype_BERT.py
+++ prototype_BERT.py
@@ -11,45 +11,52 @@ import torch.nn as nn
 from transformers import AutoModel, BertTokenizer, BertForSequenceClassification
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 
+# Загружаем датасет
+df = pd.read_csv("new.csv", index_col=0)
+df['Reviews'] = df['Reviews'].astype(str)
 
-df = pd.read_csv("new.csv")
-
+# Используем tqdm для отслеживания прогресса
 tqdm.pandas()
 
+# Разделяем данные на обучающую, валидационную и тестовую выборки
 train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
 val_df, test_df = train_test_split(temp_df, test_size=0.1, random_state=42)
 
-train_text = train_df['Reviews'].astype('str')
-train_labels = train_df[['Практика','Теория','Преподаватель','Технологии','Актуальность']]
-val_text = val_df['Reviews'].astype('str')
-val_labels = val_df[['Практика','Теория','Преподаватель','Технологии','Актуальность']]
-test_text = test_df['Reviews'].astype('str')
-test_labels = test_df[['Практика','Теория','Преподаватель','Технологии','Актуальность']]
+# Извлекаем текстовые данные и метки классов
+train_text = train_df['Reviews']
+train_labels = train_df[['Практика', 'Теория', 'Преподаватель', 'Технологии', 'Актуальность']]
+val_text = val_df['Reviews']
+val_labels = val_df[['Практика', 'Теория', 'Преподаватель', 'Технологии', 'Актуальность']]
+test_text = test_df['Reviews']
+test_labels = test_df[['Практика', 'Теория', 'Преподаватель', 'Технологии', 'Актуальность']]
+# Определяем устройство для выполнения вычислений (CPU или GPU)
+device = torch.device('cuda')
 
-device = torch.device('cpu')
-
+# Загружаем предобученную модель BERT и токенизатор
 bert = AutoModel.from_pretrained('DeepPavlov/rubert-base-cased-sentence')
 tokenizer = BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased-sentence')
 
+# Токенизация данных с добавлением паддинга и обрезки
 tokens_train = tokenizer.batch_encode_plus(
     train_text.values,
-    max_length = 50,
-    padding = 'max_length',
-    truncation = True
+    max_length=50,
+    padding='max_length',
+    truncation=True
 )
 tokens_val = tokenizer.batch_encode_plus(
     val_text.values,
-    max_length = 50,
-    padding = 'max_length',
-    truncation = True
+    max_length=50,
+    padding='max_length',
+    truncation=True
 )
 tokens_test = tokenizer.batch_encode_plus(
     test_text.values,
-    max_length = 50,
-    padding = 'max_length',
-    truncation = True
+    max_length=50,
+    padding='max_length',
+    truncation=True
 )
 
+# Преобразование токенов в тензоры
 train_seq = torch.tensor(tokens_train['input_ids'])
 train_mask = torch.tensor(tokens_train['attention_mask'])
 train_y = torch.tensor(train_labels.values)
@@ -61,24 +68,28 @@ val_y = torch.tensor(val_labels.values)
 test_seq = torch.tensor(tokens_test['input_ids'])
 test_mask = torch.tensor(tokens_test['attention_mask'])
 test_y = torch.tensor(test_labels.values)
+
+# Задаем размер батча
 batch_size = 8
 
-
+# Создаем DataLoader для обучающей и валидационной выборки
 train_data = TensorDataset(train_seq, train_mask, train_y)
 train_sampler = RandomSampler(train_data)
-train_dataloader = DataLoader(train_data, sampler = train_sampler, batch_size = batch_size)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
 
-val_data =  TensorDataset(val_seq, val_mask, val_y)
+val_data = TensorDataset(val_seq, val_mask, val_y)
 val_sampler = SequentialSampler(val_data)
-val_dataloader = DataLoader(val_data, sampler = val_sampler, batch_size = batch_size)
+val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
 
+# Замораживаем параметры модели BERT, чтобы они не обновлялись при обучении
 for param in bert.parameters():
     param.requires_grad = False
 
 
+# Определяем архитектуру модели на основе BERT
 class BERT_Arch(nn.Module):
 
-    def __init__(self, bert):
+    def __init__(self, bert: AutoModel.from_pretrained):
         super(BERT_Arch, self).__init__()
         self.bert = bert
         self.dropout = nn.Dropout(0.1)
@@ -88,6 +99,7 @@ class BERT_Arch(nn.Module):
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, sent_id, mask):
+        # Извлекаем CLS-токен для классификации
         _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
         x = self.fc1(cls_hs)
         x = self.relu(x)
@@ -97,32 +109,37 @@ class BERT_Arch(nn.Module):
         return x
 
 
+# Инициализируем модель и перемещаем на устройство
 model = BERT_Arch(bert)
-
 model = model.to(device)
-from transformers import AdamW
 
+from torch.optim import AdamW
+
+# Определяем оптимизатор
 optimizer = AdamW(model.parameters(),
-               lr= 1e-3)
-
+                  lr=1e-3)
 
 from sklearn.utils.class_weight import compute_class_weight
 
+# Рассчитываем веса классов для учета дисбаланса
 class_weights = compute_class_weight(
-                                        class_weight = "balanced",
-                                        classes = np.unique(train_labels),
-                                        y = train_labels.values.reshape(-1)
-                                    )
-
+    class_weight="balanced",
+    classes=np.unique(train_labels),
+    y=train_labels.values.reshape(-1)
+)
 
 print(class_weights)
 
-weights = torch.tensor(class_weights, dtype = torch.float)
+# Преобразуем веса классов в тензор и перемещаем на устройство
+weights = torch.tensor(class_weights, dtype=torch.float)
 weights = weights.to(device)
 cross_entropy = nn.CrossEntropyLoss()
+
+# Задаем количество эпох
 epochs = 20
 
 
+# Функция обучения модели
 def train():
     model.train()
     total_loss, total_accuracy = 0, 0
@@ -131,13 +148,13 @@ def train():
     for step, batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
         batch = [r.to(device) for r in batch]
         sent_id, mask, labels = batch
-        labels = labels.float()
+        labels = labels.float()  # Приводим метки к float для расчета потерь
         model.zero_grad()
         preds = model(sent_id, mask)
-        loss = nn.BCEWithLogitsLoss()(preds, labels)
+        loss = nn.BCEWithLogitsLoss()(preds, labels)  # Используем бинарную кросс-энтропию с логитами
         total_loss += loss.item()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)  # Ограничиваем градиенты для стабильности
         optimizer.step()
         preds = preds.detach().cpu().numpy()
         total_preds.append(preds)
@@ -148,6 +165,7 @@ def train():
     return avg_loss, total_preds
 
 
+# Функция оценки модели
 def evaluate():
     model.eval()
     total_loss, total_accuracy = 0, 0
@@ -159,7 +177,7 @@ def evaluate():
 
         with torch.no_grad():
             preds = model(sent_id, mask)
-            loss = cross_entropy(preds, labels.float())
+            loss = cross_entropy(preds, labels.float())  # Применяем cross-entropy для оценки
             total_loss = total_loss + loss.item()
             preds = preds.detach().cpu().numpy()
             total_preds.append(preds)
@@ -176,25 +194,27 @@ best_valid_loss = float('inf')
 train_losses = []
 valid_losses = []
 
+# Основной цикл обучения модели
 for epoch in range(epochs):
     print('\n Epoch{:} / {:}'.format(epoch + 1, epochs))
 
     train_loss, _ = train()
-    valid_loss, valid_preds = evaluate() # Получаем предсказания
+    valid_loss, valid_preds = evaluate()  # Получаем предсказания
 
     if valid_loss < best_valid_loss:
         best_valid_loss = valid_loss
-        torch.save(model.state_dict(), 'saved_weights.pt')
+        torch.save(model.state_dict(), 'saved_weights.pt')  # Сохраняем лучшие веса модели
 
     train_losses.append(train_loss)
     valid_losses.append(valid_loss)
     print(f'\nTraining loss: {train_loss:.3f}')
     print(f'Validation loss: {valid_loss:.3f}')
 
-
+# Загрузка наилучших сохраненных весов модели
 path = 'saved_weights.pt'
 model.load_state_dict(torch.load(path))
 
+# Пример использования модели на новых данных
 predictions = ["Крутой преподователь"]
 with torch.no_grad():
     preds = model(test_seq, test_mask)
