ff393b16c03de0d37b8cb5826b32c1418bea2dd7
GuruProger <progerguru@gmail.com>
2024-08-22 21:02:15 +0300

    feat(bot): added /help command
    - Added new file `exemple_answer.csv`
    - Added new file `exemple_send.csv`
    - Updated command handler in `handler.py`
    - Added support for /help command in `main.py`

--- bot/core/handlers/handler.py
+++ bot/core/handlers/handler.py
@@ -1,7 +1,32 @@
 from aiogram import Bot
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
+
+from pathlib import Path
+
+# Получаем путь к текущему файлу и строим путь к нужному файлу
+current_dir = Path(__file__).resolve().parent
+path_data = current_dir.parent.parent / 'core' / 'data'
 
 
 # Команда /start
 async def cmd_start(message: Message, bot: Bot):
-	await message.answer("Привет! Отправьте мне CSV файл.")
+	await message.answer(
+		"Привет! Ты мне можешь отправить текст или CSV файл, если что-то непонятно, то используй команду /help")
+
+
+# Команда /help
+async def cmd_help(message: Message, bot: Bot):
+	await message.answer("""
+Я размечаю <s>текст</s> отзывы по следующим аспектам: <b>практика, теория, преподаватель, технологии, актуальность</b>.
+
+Для работы необходимо отправить текст в бота, если вам нужно разметить сразу несколько текстов, то отправьте csv файл
+""")
+	# Получение файла
+	send_file_path = path_data / 'exemple_send.csv'
+	answer_file_path = path_data / 'exemple_answer.csv'
+
+	document_send = FSInputFile(send_file_path)
+	document_answer = FSInputFile(answer_file_path)
+
+	await message.answer_document(document=document_send, caption='Отправьте мне CSV файл по такому шаблону')
+	await message.answer_document(document=document_answer, caption='Я размечу его и отправлю назад в таком виде')
--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,10 @@ import logging
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, ContentType
 from aiogram.filters import Command
-from core.handlers.handler import cmd_start
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
+
+from core.handlers.handler import cmd_start, cmd_help
 from core.handlers.basic import handle_csv_upload
 
 load_dotenv()
@@ -26,7 +29,8 @@ async def stop_bot(bot: Bot):
 
 # Функция запуска бота
 async def main():
-	bot = Bot(token=token)  # Создаем объект бота с заданным токеном
+		# Создаем объект бота с заданным токеном, присваиваем HTML parse_mode для использования  HTML тегов в сообщениях
+	bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 	dp = Dispatcher()  # Создаем диспетчер для обработки входящих сообщений и команд
 
@@ -34,6 +38,7 @@ async def main():
 	dp.shutdown.register(stop_bot)  # Функция срабатывает при остановке бота
 
 	dp.message.register(cmd_start, Command(commands=['start']))
+	dp.message.register(cmd_help, Command(commands=['help']))
 
 	dp.message.register(handle_csv_upload, F.document)
 	# dp.message.handlers(handle_csv_upload, content_types=ContentType.DOCUMENT)
