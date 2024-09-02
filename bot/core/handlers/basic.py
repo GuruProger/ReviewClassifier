7c9189f2b3d2e6b311da3fb12262224464451d70
GuruProger <progerguru@gmail.com>
2024-09-02 09:18:53 +0300

    feat(bot): enhance CSV and text message handlers with API integration and error handling
    - Updated `handle_csv_upload` to process CSV files in memory and send them to the API for predictions.
    - Updated `handle_text_message` to interact with the API for text processing and respond with predictions.
    - Added comprehensive error handling for file type validation and API response errors.

--- bot/core/handlers/basic.py
+++ bot/core/handlers/basic.py
@@ -1,21 +1,52 @@
+import io
+import requests
 from aiogram import Bot
-from aiogram.types import Message
+from aiogram.types import Message, BufferedInputFile
+from ..utils.model_prediction import predict_file, predict_text
 
 
 # Обработчик загрузки CSV файла
 async def handle_csv_upload(message: Message, bot: Bot):
 	document = message.document
 
-	if document.mime_type == 'text/csv':
-		file_path = await bot.download(document)
-		await message.answer('Файл получен')
-		...
-
-	# Завершаем состояние ожидания файла
-	else:
+	if document.mime_type != 'text/csv':
 		await message.answer("Пожалуйста, загрузите CSV файл.")
+	else:
+		# Создаем объект BytesIO для хранения содержимого файла в памяти
+		file_in_memory = io.BytesIO()
+
+		# Загружаем файл в объект BytesIO
+		await bot.download(document, destination=file_in_memory)
+
+		# Перемещаем указатель в начало файла
+		file_in_memory.seek(0)
+		await message.answer('Файл успешно получен')
+
+		result = predict_file((document.file_name, file_in_memory, 'text/csv'))
+
+		# Если результат содержит ошибку, вернуть сообщение с ошибкой
+		if isinstance(result, dict) and "error_code" in result:
+			error_message = f"Ошибка при предсказании: {result['error_message']}"
+			if result['error_code']:
+				error_message += f" (Код ошибки: {result['error_code']})"
+			await message.answer(error_message)
+			return
+
+		result.seek(0)
+
+		await message.answer_document(
+			document=BufferedInputFile(result.read(), document.file_name.split('.')[0] + '_predictions.csv'))
 
 
 # Обработчик отправки текстового сообщения
 async def handle_text_message(message: Message, bot: Bot):
-	...
+	result = predict_text(message.text)
+	if "error_code" in result:
+		await message.reply(f"Ошибка {result['error_code']}: {result['error_message']}")
+	else:
+		text_aspect = [val for i, val in enumerate(result) if val != 'Reviews' and result[val]]
+		text_answer = f"Ответ от <b>API</b>:\n{result}\n\n"
+		if text_aspect:
+			text_answer += f"Предсказанные аспекты: {', '.join(text_aspect)}\n"
+
+		await message.reply(text_answer)
---
+++ bot/core/utils/model_prediction.py
@@ -0,0 +1,38 @@
+import requests
+import io
+
+API_URL = "http://localhost:8001"
+
+
+def predict_text(text: str):
+	endpoint = f"{API_URL}/predict/text"
+	response = None
+
+	try:
+		payload = {"text": text}
+		response = requests.post(endpoint, json=payload)
+		response.raise_for_status()  # Проверка на успешный статус-код (200-299)
+		return response.json()
+
+	except requests.exceptions.HTTPError as http_err:
+		error_code = response.status_code if response else None
+		return {"error_code": error_code, "error_message": str(http_err)}
+
+	except Exception as err:
+		return {"error_code": None, "error_message": str(err)}
+
+
+def predict_file(file):
+	endpoint = f"{API_URL}/predict/file"
+	response = None
+	try:
+		response = requests.post(endpoint, files={'csv_file': file})
+		response.raise_for_status()  # Проверка на успешный статус-код (200-299)
+
+		return io.BytesIO(response.content)
+	except requests.exceptions.HTTPError as http_err:
+		error_code = response.status_code if response else None
+		return {"error_code": error_code, "error_message": str(http_err)}
+
+	except Exception as err:
+		return {"error_code": None, "error_message": str(err)}
