573690812edbb3ae167183575215baaa68429e33
GuruProger <progerguru@gmail.com>
2024-09-09 09:40:12 +0300

    refactor(api): moved constants to separate variables Refactoring: key strings are now declared as variables at the beginning of the code. This improves readability and makes it easier to update the values.

--- api/database.py
+++ api/database.py
@@ -4,148 +4,148 @@ import secrets
 
 
 # Класс для работы с базой данных
-class DatabaseClient():
-    def __init__(self):
-        # Инициализация и подключение к базе данных при создании объекта
-        self.connection = self.connect_to_database()
-        self.connection.execute('PRAGMA journal_mode=WAL;')
+class DatabaseClient:
+	def __init__(self):
+		# Инициализация и подключение к базе данных при создании объекта
+		self.connection = self.connect_to_database()
+		self.connection.execute('PRAGMA journal_mode=WAL;')
 
-    # Метод, вызываемый при входе в контекстный менеджер (with)
-    def __enter__(self):
-        return self
+	# Метод, вызываемый при входе в контекстный менеджер (with)
+	def __enter__(self):
+		return self
 
-    # Метод, вызываемый при выходе из контекстного менеджера (with)
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # Закрытие соединения с базой данных
-        self.connection.close()
+	# Метод, вызываемый при выходе из контекстного менеджера (with)
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		# Закрытие соединения с базой данных
+		self.connection.close()
 
-    # Метод для подключения к базе данных
-    def connect_to_database(self, db_name="api/Feedback_AI.db"):
-        # Устанавливаем соединение с базой данных SQLite
-        self.conn = sqlite3.connect(db_name, timeout=3)
-        # Возвращаем курсор для выполнения SQL-запросов
-        return self.conn.cursor()
+	# Метод для подключения к базе данных
+	def connect_to_database(self, db_name="api/Feedback_AI.db"):
+		# Устанавливаем соединение с базой данных SQLite
+		self.conn = sqlite3.connect(db_name, timeout=3)
+		# Возвращаем курсор для выполнения SQL-запросов
+		return self.conn.cursor()
 
-    # Метод для явного закрытия соединения с базой данных
-    def close(self):
-        self.conn.close()
+	# Метод для явного закрытия соединения с базой данных
+	def close(self):
+		self.conn.close()
 
-    # Метод для получения пользователя по уникальному ключу
-    def get_user_by_key(self, key):
-        try:
-            # Выполняем SQL-запрос для поиска пользователя по ключу
-            result = self.connection.execute("""SELECT id, current_plan FROM users
+	# Метод для получения пользователя по уникальному ключу
+	def get_user_by_key(self, key):
+		try:
+			# Выполняем SQL-запрос для поиска пользователя по ключу
+			result = self.connection.execute("""SELECT id, current_plan FROM users
                                                 WHERE hash_key = ?""", (key,)).fetchone()
-            return result
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение и возвращаем None
-            print(f"Error getting user by key: {e}")
-            return None
+			return result
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение и возвращаем None
+			print(f"Error getting user by key: {e}")
+			return None
 
-    # Метод для обновления статистики курса
-    def update_statistics_for_course(self, owner, course_key, new_stat):
-        try:
-            # Получаем текущую статистику курса из базы данных
-            course_statistics = self.connection.execute("""SELECT feedback_statistics FROM courses
+	# Метод для обновления статистики курса
+	def update_statistics_for_course(self, owner, course_key, new_stat):
+		try:
+			# Получаем текущую статистику курса из базы данных
+			course_statistics = self.connection.execute("""SELECT feedback_statistics FROM courses
                                                            WHERE key = ? AND owner = ?""",
-                                                        (course_key, owner)).fetchone()
-            if course_statistics:
-                # Преобразуем статистику из JSON-строки в словарь
+														(course_key, owner)).fetchone()
+			if course_statistics:
+				# Преобразуем статистику из JSON-строки в словарь
 
-                processed_course_statistics = json.loads(course_statistics[0])
-                # Обновляем значения статистики на основе новых данных
-                new_data = {
-                                "practice": int(new_stat[0]),
-                                "theory": int(new_stat[1]),
-                                "teacher": int(new_stat[2]),
-                                "technology": int(new_stat[3]),
-                                "relevance": int(new_stat[4])
-                            }
-                for key in new_data:
-                    if key in processed_course_statistics:
-                        print( processed_course_statistics)
-                        print(new_data)
-                        print(key)
-                        print(new_data["practice"])
-                        print(type(processed_course_statistics))
-                        print(processed_course_statistics["practice"])
-                        processed_course_statistics[key] += new_data[key]
-                # Сохраняем обновленную статистику обратно в базу данных
-                self.connection.execute("""UPDATE courses SET feedback_statistics = ? 
+				processed_course_statistics = json.loads(course_statistics[0])
+				# Обновляем значения статистики на основе новых данных
+				new_data = {
+					"practice": int(new_stat[0]),
+					"theory": int(new_stat[1]),
+					"teacher": int(new_stat[2]),
+					"technology": int(new_stat[3]),
+					"relevance": int(new_stat[4])
+				}
+				for key in new_data:
+					if key in processed_course_statistics:
+						print(processed_course_statistics)
+						print(new_data)
+						print(key)
+						print(new_data["practice"])
+						print(type(processed_course_statistics))
+						print(processed_course_statistics["practice"])
+						processed_course_statistics[key] += new_data[key]
+				# Сохраняем обновленную статистику обратно в базу данных
+				self.connection.execute("""UPDATE courses SET feedback_statistics = ? 
                                            WHERE key = ? AND owner = ?""",
-                                        (json.dumps(processed_course_statistics), course_key, owner))
-                self.conn.commit()  # Фиксируем изменения в базе данных
-                return 1
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение
-            print(f"Error updating statistics for course: {e}")
-            return
+										(json.dumps(processed_course_statistics), course_key, owner))
+				self.conn.commit()  # Фиксируем изменения в базе данных
+				return 1
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение
+			print(f"Error updating statistics for course: {e}")
+			return
 
-    # Метод для получения статистики по курсу
-    def get_statistics_for_course(self, owner, course_key):
-        try:
-            # Выполняем SQL-запрос для получения статистики курса
-            course_statistics = self.connection.execute("""SELECT feedback_statistics FROM courses
+	# Метод для получения статистики по курсу
+	def get_statistics_for_course(self, owner, course_key):
+		try:
+			# Выполняем SQL-запрос для получения статистики курса
+			course_statistics = self.connection.execute("""SELECT feedback_statistics FROM courses
                                                            WHERE key = ? AND owner = ?""",
-                                                        (course_key, owner)).fetchone()
-            # Возвращаем статистику или None, если курс не найден
-            return course_statistics[0] if course_statistics else None
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение и возвращаем None
-            print(f"Error getting statistics for course: {e}")
-            return None
+														(course_key, owner)).fetchone()
+			# Возвращаем статистику или None, если курс не найден
+			return course_statistics[0] if course_statistics else None
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение и возвращаем None
+			print(f"Error getting statistics for course: {e}")
+			return None
 
-    # Метод для добавления нового курса
-    def add_course(self, owner):
-        try:
-            # Генерируем уникальный ключ для курса
-            key = secrets.token_hex(2)
-            # Выполняем SQL-запрос для добавления нового курса в базу данных
-            self.connection.execute("""INSERT INTO courses (key, owner) VALUES (?, ?)""", (key, owner))
-            self.conn.commit()  # Фиксируем изменения в базе данных
-            return key  # Возвращаем ключ нового курса
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение и возвращаем None
-            print(f"Error adding course: {e}")
-            return None
+	# Метод для добавления нового курса
+	def add_course(self, owner):
+		try:
+			# Генерируем уникальный ключ для курса
+			key = secrets.token_hex(2)
+			# Выполняем SQL-запрос для добавления нового курса в базу данных
+			self.connection.execute("""INSERT INTO courses (key, owner) VALUES (?, ?)""", (key, owner))
+			self.conn.commit()  # Фиксируем изменения в базе данных
+			return key  # Возвращаем ключ нового курса
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение и возвращаем None
+			print(f"Error adding course: {e}")
+			return None
 
-    # Метод для добавления нового пользователя
-    def add_user(self, tg_id):
-        try:
-            # Генерируем уникальный ключ (хэш) для пользователя
-            hash_key = secrets.token_hex(16)
-            user = self.connection.execute("""SELECT id FROM users
+	# Метод для добавления нового пользователя
+	def add_user(self, tg_id):
+		try:
+			# Генерируем уникальный ключ (хэш) для пользователя
+			hash_key = secrets.token_hex(16)
+			user = self.connection.execute("""SELECT id FROM users
                                                             WHERE tg_id = ?""", (tg_id,)).fetchone()
-            if user:
-                return "Пользователь уже существует"
-            # Выполняем SQL-запрос для добавления нового пользователя в базу данных
-            self.connection.execute("""INSERT INTO users (tg_id, hash_key) VALUES (?, ?)""", (tg_id, hash_key))
-            self.conn.commit()  # Фиксируем изменения в базе данных
-            return hash_key  # Возвращаем сгенерированный ключ
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение и возвращаем None
-            print(f"Error adding user: {e}")
-            return None
+			if user:
+				return "Пользователь уже существует"
+			# Выполняем SQL-запрос для добавления нового пользователя в базу данных
+			self.connection.execute("""INSERT INTO users (tg_id, hash_key) VALUES (?, ?)""", (tg_id, hash_key))
+			self.conn.commit()  # Фиксируем изменения в базе данных
+			return hash_key  # Возвращаем сгенерированный ключ
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение и возвращаем None
+			print(f"Error adding user: {e}")
+			return None
 
-    # Метод для удаления курса
-    def del_course(self, owner, course_key):
-        try:
-            # Выполняем SQL-запрос для удаления курса из базы данных
-            self.connection.execute("""DELETE FROM courses WHERE key = ? AND owner = ?""", (course_key, owner))
-            self.conn.commit()  # Фиксируем изменения в базе данных
-            return "ok"  # Возвращаем строку "ok" при успешном удалении
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение и возвращаем None
-            print(f"Error deleting course: {e}")
-            return None
+	# Метод для удаления курса
+	def del_course(self, owner, course_key):
+		try:
+			# Выполняем SQL-запрос для удаления курса из базы данных
+			self.connection.execute("""DELETE FROM courses WHERE key = ? AND owner = ?""", (course_key, owner))
+			self.conn.commit()  # Фиксируем изменения в базе данных
+			return "ok"  # Возвращаем строку "ok" при успешном удалении
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение и возвращаем None
+			print(f"Error deleting course: {e}")
+			return None
 
-    def get_user_by_tg(self, key):
-        try:
-            # Выполняем SQL-запрос для поиска пользователя по ключу
-            result = self.connection.execute("""SELECT hash_key, current_plan FROM users
+	def get_user_by_tg(self, key):
+		try:
+			# Выполняем SQL-запрос для поиска пользователя по ключу
+			result = self.connection.execute("""SELECT hash_key, current_plan FROM users
                                                 WHERE tg_id = ?""", (key,)).fetchone()
-            return result
-        except sqlite3.Error as e:
-            # В случае ошибки выводим сообщение и возвращаем None
-            print(f"Error getting user by key: {e}")
-            return None
+			return result
+		except sqlite3.Error as e:
+			# В случае ошибки выводим сообщение и возвращаем None
+			print(f"Error getting user by key: {e}")
+			return None
--- api/test.py
+++ api/test.py
@@ -1,87 +1,89 @@
 import requests
 
 # Базовый URL для вашего API
-base_url = "http://localhost:8000"
+BASE_URL = "http://localhost:8000"
+PLATFORM_KEY = "ebfbwiebflbsvjhbjehf"
+SECRET_KEY = "23b301fe34fecfb712ee62fd33069686"
+
 
 def test_review_from_list():
-    data = {
-        "platform_key": "ebfbwiebflbsvjhbjehf",  # Замените на действительный ключ платформы
-        "course_key": "t0ZH",
-        "review": ['Преподователь топ, теория тоже. Практика просто офигенная']
-    }
-    response = requests.post(f"{base_url}/predict/text", data=data)
-    print("POST /predict/text:", response.json())
+	data = {
+		"platform_key": PLATFORM_KEY,  # Замените на действительный ключ платформы
+		"course_key": "t0ZH",
+		"review": ['Преподователь топ, теория тоже. Практика просто офигенная']
+	}
+	response = requests.post(f"{BASE_URL}/predict/text", data=data)
+	print("POST /predict/text:", response.json())
 
 
 def test_review_from_file():
-    file_path = 'test_reviews.csv'
+	file_path = 'test_reviews.csv'
 
-    with open(file_path, 'rb') as file:
-        files = {'file': (file_path, file, 'text/csv')}
-        data = {
-            "platform_key": "ebfbwiebflbsvjhbjehf",  # Замените на действительный ключ платформы
-            "course_key": "t0ZH"
-        }
-        response = requests.post(f"{base_url}/predict/file", files=files, data=data)
+	with open(file_path, 'rb') as file:
+		files = {'file': (file_path, file, 'text/csv')}
+		data = {
+			"platform_key": PLATFORM_KEY,  # Замените на действительный ключ платформы
+			"course_key": "t0ZH"
+		}
+		response = requests.post(f"{BASE_URL}/predict/file", files=files, data=data)
 
-        # Проверка успешности запроса
-        if response.status_code == 200:
-            print('Success:', response.json())
-        else:
-            print('Failed:', response.status_code, response.text)
+		# Проверка успешности запроса
+		if response.status_code == 200:
+			print('Success:', response.json())
+		else:
+			print('Failed:', response.status_code, response.text)
 
 
 def test_statistics():
-    params = {
-        "platform_key": "ebfbwiebflbsvjhbjehf",  # Замените на действительный ключ платформы
-        "course_key": "t0ZH"  # Замените на действительный ключ курса
-    }
-    response = requests.get(f"{base_url}/statistics", params=params)
-    print("GET /statistics:", response.json())
+	params = {
+		"platform_key": PLATFORM_KEY,  # Замените на действительный ключ платформы
+		"course_key": "t0ZH"  # Замените на действительный ключ курса
+	}
+	response = requests.get(f"{BASE_URL}/statistics", params=params)
+	print("GET /statistics:", response.json())
 
 
 def test_add_course():
-    data = {
-        "platform_key": "ebfbwiebflbsvjhbjehf"  # Замените на действительный ключ платформы
-    }
-    response = requests.post(f"{base_url}/course", data=data)
-    print("POST /course:", response.json())
+	data = {
+		"platform_key": PLATFORM_KEY  # Замените на действительный ключ платформы
+	}
+	response = requests.post(f"{BASE_URL}/course", data=data)
+	print("POST /course:", response.json())
 
 
 def test_del_course():
-    data = {
-        "platform_key": "ebfbwiebflbsvjhbjehf",  # Замените на действительный ключ платформы
-        "course_key": "ZVPU"  # Замените на действительный ключ курса
-    }
-    response = requests.delete(f"{base_url}/course", data=data)
-    print("DELETE /course:", response.json())
-
+	data = {
+		"platform_key": PLATFORM_KEY,  # Замените на действительный ключ платформы
+		"course_key": "ZVPU"  # Замените на действительный ключ курса
+	}
+	response = requests.delete(f"{BASE_URL}/course", data=data)
+	print("DELETE /course:", response.json())
 
 
 def test_add_user():
-    data = {
-        "secret_key": "23b301fe34fecfb712ee62fd33069686",  # Замените на действительный ключ
-        "tg_key": "654321"  # Замените на действительный ключ
-    }
-    response = requests.post(f"{base_url}/add_user", data=data)
-    print("POST /add_user:", response.json())
+	data = {
+		"secret_key": SECRET_KEY,  # Замените на действительный ключ
+		"tg_key": "654321"  # Замените на действительный ключ
+	}
+	response = requests.post(f"{BASE_URL}/add_user", data=data)
+	print("POST /add_user:", response.json())
 
 
 def test_get_user():
-    params = {
-        "secret_key": "23b301fe34fecfb712ee62fd33069686",  # Замените на действительный ключ
-        "tg_id": "123456"  # Замените на действительный TG ID
-    }
-    response = requests.get(f"{base_url}/get_user", params=params)
-    print("GET /get_user:", response.json())
+	params = {
+		"secret_key": SECRET_KEY,  # Замените на действительный ключ
+		"tg_id": "123456"  # Замените на действительный TG ID
+	}
+	response = requests.get(f"{BASE_URL}/get_user", params=params)
+	print("GET /get_user:", response.json())
 
 
 # Запуск тестов
 if __name__ == "__main__":
-    test_review_from_list()
-    #test_review_from_file()
-    test_statistics()
-    test_add_course()
-    test_del_course()
-    test_add_user()
-    test_get_user()
+	test_review_from_list()
+	# test_review_from_file()
+	test_statistics()
+	test_add_course()
+	test_del_course()
+	test_add_user()
+	test_get_user()
