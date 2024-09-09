8a477a3475cdcf0902c59b7acd6ed454d8cf4270
GuruProger <progerguru@gmail.com>
2024-09-09 13:57:26 +0300

    feat(bot): add API key issuance on /start command
    Added a feature where the Telegram bot issues an API key when the /start command is used. If the key already exists, the bot displays the existing key.

--- bot/core/handlers/handler.py
+++ bot/core/handlers/handler.py
@@ -1,7 +1,7 @@
 from aiogram import Bot
 from aiogram.types import Message, FSInputFile
-
 from pathlib import Path
+from ..utils.api_requests import add_user, get_user
 
 # Получаем путь к текущему файлу и строим путь к нужному файлу
 current_dir = Path(__file__).resolve().parent
@@ -12,6 +12,16 @@ path_data = current_dir.parent.parent / 'core' / 'data'
 async def cmd_start(message: Message, bot: Bot):
 	await message.answer(
 		"Привет! Ты мне можешь отправить текст или CSV файл, если что-то непонятно, то используй команду /help")
+
+	response = get_user(message.chat.id)
+	if response and 'status' in response and response['status'] == 'success':
+		await message.answer('Твой api-ключ: ' + response['user'][0])
+		return
+
+	# Если у пользователя нет api-ключа, то добавляем выдаём его
+	response = add_user(message.chat.id)
+	if response and response['status'] == 'success':
+		await message.answer('Тебе выдан api-ключ: ' + response['key'])
 
 
 # Команда /help
---
+++ bot/core/utils/api_requests.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import os
+import requests
+import httpx
+
+load_dotenv()
+secret_key = os.getenv("SECRET_KEY")
+BASE_URL = "http://localhost:8000"
+
+
+def add_user(user_id: int | str):
+	data = {
+		"secret_key": secret_key,
+		"tg_key": user_id
+	}
+
+	try:
+		response = requests.post(f"{BASE_URL}/add_user", data=data)
+		response.raise_for_status()
+		return response.json()
+	except httpx.HTTPStatusError as e:
+		# Обработка ошибки отправки
+		print(f"Error: {e.response.text}")
+		return {'status': 'error'}
+
+
+def get_user(user_id: int | str):
+	params = {
+		"secret_key": secret_key,
+		"tg_id": user_id
+	}
+	try:
+		response = requests.get(f"{BASE_URL}/get_user", params=params)
+
+		response.raise_for_status()
+		return response.json()
+
+	except requests.exceptions.HTTPError as e:
+		if e.response.status_code == 404:
+			return {'status': 'error', 'message': 'User not found'}
+		else:
+			return {'status': 'error', 'message': 'HTTP error'}
+	except httpx.HTTPStatusError as e:
+		return {'status': 'error', 'message': e.response.text}
+
+
+# print(add_user('1234156'))
+# print(get_user('12312312312'))
